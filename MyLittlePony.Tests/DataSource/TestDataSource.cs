37f91115d557d7b6f63b774704a67df9ce525d55
mme <mme@softserveinc.com>
2023-03-14 17:50:35 +0200

    Add project files.

---
+++ MyLittlePony.Tests/DataSource/TestDataSource.cs
@@ -0,0 +1,22 @@
+﻿using MyLittlePony.AT.Framework.Models;
+using NUnit.Framework;
+using System.Collections;
+
+namespace MyLittlePony.Tests.DataSource
+{
+    public static class TestDataSource
+    {
+        public static IEnumerable LoginTestCases
+        {
+            get
+            {
+                yield return new TestCaseData(new LoginInfo() { UserName = "mykola", Password = "tuqd3" })
+                    .SetCategory("Smoke")
+                    .SetName("{m}ViaUserNameTest");
+                yield return new TestCaseData(new LoginInfo() { UserName = "melnykmykola1991@gmail.com", Password = "tuqd3" })
+                    .SetCategory("Regression")
+                    .SetName("{m}ViaEmailTest");
+            }
+        }
+    }
+}
